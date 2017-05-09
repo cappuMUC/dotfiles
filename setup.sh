744b31f671a65d0595212393e0848f9b2ed58860
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2017-05-09 20:42:04 +0200

    fixed script path if invoked from outside

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-LOCAL_DIR=$('pwd');
-DEIN_DOT_VIM_PATH="$HOME/.vim/bundles"
+#determine path of dotfiles repository
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_FOLDER=$(dirname $SCRIPT_PATH)
 
+# create folder for dein.vim plugin manager
+DEIN_DOT_VIM_PATH="$HOME/.vim/bundles"
 mkdir -p $DEIN_DOT_VIM_PATH
 
 
@@ -23,5 +26,5 @@ sh ./installer.sh $DEIN_DOT_VIM_PATH
 rm installer.sh
 
 # create symlinks for dotfiles
-ln -s $LOCAL_DIR/vim/vimrc $HOME/.vimrc
-ln -s $LOCAL_DIR/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $SCRIPT_FOLDER/vim/vimrc $HOME/.vimrc
+ln -s $SCRIPT_FOLDER/tmux/tmux.conf $HOME/.tmux.conf
